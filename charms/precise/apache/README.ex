ebf2a820466d05c28a00cb94c5445be489eac012
Mark Mims <mark.mims@canonical.com>
2013-09-27 11:17:13 -0600

    adding a private charm

---
+++ charms/precise/apache/README.ex
@@ -0,0 +1,41 @@
+Describe the intended usage of this charm and anything unique about how
+this charm relates to others here. 
+
+This README will be displayed in the Charm Store, it should be either Markdown or RST. Ideal READMEs include instructions on how to use the charm, expected usage, and charm features that your audience might be interested in. For an example of a well written README check out Hadoop: http://jujucharms.com/charms/precise/hadoop
+
+Here's an example you might wish to template off of:
+
+Overview
+--------
+
+This charm provides (service) from (service homepage). Add a description here of what the service itself actually does. 
+
+
+Usage
+-----
+
+Step by step instructions on using the charm:
+
+    juju deploy servicename
+
+and so on. If you're providing a web service or something that the end user needs to go to, tell them here, especially if you're deploying a service that might listen to a non-default port. 
+
+You can then browse to http://ip-address to configure the service. 
+
+Configuration
+-------------
+
+The configuration options will be listed on the charm store, however If you're making assumptions or opinionated decisions in the charm (like setting a default administrator password), you should detail that here so the user knows how to change it immediately, etc.
+
+
+Contact Information
+-------------------
+
+Though this will be listed in the charm store itself don't assume a user will know that, so include that information here:
+
+Author:
+Report bugs at: http://bugs.launchpad.net/charms/+source/charmname
+Location: http://jujucharms.com/charms/distro/charmname
+
+* Be sure to remove the templated parts before submitting to https://launchpad.net/charms for inclusion in the charm store.
+
